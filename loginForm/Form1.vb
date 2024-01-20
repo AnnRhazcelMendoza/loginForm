03356a69a05f2994f0b19eac4785a834aebd8aab
Administrator <Administrator@COMLAB01>
2024-01-20 11:52:22 +0700

    Add project files.

---
+++ loginForm/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "admin" And TextBox2.Text = "admin12" Then
+
+            Form2.Show()
+
+        Else
+            MsgBox("wrong username or password")
+        End If
+    End Sub
+End Class
